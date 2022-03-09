1fde687047ffbcde9ce50bae0803c15e3980a96a
estudiante <estudiante@pc6>
2022-03-09 12:04:16 +0000

    BlockNotas, Actualizacion 1

---
+++ JAVA_VICENTE/src/BlockNotas/NotaAlarma.java
@@ -0,0 +1,12 @@
+package BlockNotas;
+
+import java.time.LocalDateTime;
+
+public class NotaAlarma  extends Nota{
+
+	public NotaAlarma(String nota, LocalDateTime fecha, boolean igual) {
+		super(nota);
+		
+	}
+
+}
